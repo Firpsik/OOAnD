b504032c65c79b5a5b56f6d025293111ddd21b3e
Ilia <126543228+weoore@users.noreply.github.com>
2024-09-25 09:04:13 +0600

    cover

--- SpaceBattle/Vector.cs
+++ SpaceBattle/Vector.cs
@@ -11,19 +11,7 @@ public class Vector
 
     public static Vector operator +(Vector v1, Vector v2)
     {
-        {
-            if (v1 == null || v2 == null)
-            {
-                throw new ArgumentNullException("Оба вектора должны быть не null");
-            }
-
-            if (v1.nums.Length != v2.nums.Length)
-            {
-                throw new ArgumentException("вектор должен быть той же размерности");
-            }
-
-            return new Vector(v1.nums.Select((num, i) => num + v2.nums[i]).ToArray());
-        }
+        return new Vector(v1.nums.Select((num, i) => num + v2.nums[i]).ToArray());
     }
     public override bool Equals(object? obj)
     {
