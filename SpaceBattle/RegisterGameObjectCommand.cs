088be7fdfed7a6a4dcd743e0aaccd474349c933b
Ilia <126543228+weoore@users.noreply.github.com>
2025-02-05 20:19:17 +0600

    add test

---
+++ SpaceBattle/RegisterGameObjectCommand.cs
@@ -0,0 +1,18 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class RegisterGameObjectCommand : ICommand
+{
+    private readonly IUObject _obj;
+    private readonly object _id;
+    public RegisterGameObjectCommand(IUObject obj, object id)
+    {
+        _obj = obj;
+        _id = id;
+    }
+    public void Execute()
+    {
+        IoC.Resolve<IDictionary<object, IUObject>>("GameObjects").Add(_id, _obj);
+    }
+}
