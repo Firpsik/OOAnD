5776e6116f6f900923e098b310b931a9671241ce
Dmitry <dimagg51@mail.ru>
2025-03-19 11:49:12 +0600

    add DependencyInjection class

---
+++ SpaceBattle/Reflection.cs
@@ -0,0 +1,19 @@
+using Hwdtech;
+
+namespace SpaceBattle;
+
+public static class DependencyInjection
+{
+    public static object GetInstance(Type type)
+    {
+        _ = type ?? throw new ArgumentNullException(nameof(type));
+
+        var constructor = type.GetConstructors().FirstOrDefault() 
+            ?? throw new InvalidOperationException($"No public constructors found for type: {type}");
+
+        var parameters = constructor.GetParameters()
+            .Select(p => IoC.Resolve<object>($"{p.ParameterType}")).ToArray();
+
+        return constructor.Invoke(parameters);
+    }
+}
